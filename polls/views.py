914781f97224a197d989e26d00dabeace92de2b1
61246880 <jorge.plautz@gmail.com>
2020-05-05 00:14:33 -0300

    Criado django admin
    close #12

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# from django.template import loader
 
 # Create your views here.
-from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("<h1>Hello, world. You're at the polls index.</h1>")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+# segunda apresentação
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# primeira apresentação
+# # def index(request):
+#     return HttpResponse("<h1>Hello, world. You're at the polls index.</h1>")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
